0870084da9016418b7618691758931542dd874ed
Lsning-OIG <83625894+Lsning-OIG@users.noreply.github.com>
2022-03-21 13:52:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  title: '乐乐的网站',
+  githubName: 'Lsning-OIG',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '乐乐的博客，零境的世界',
+  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,乐乐,乐乐的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
